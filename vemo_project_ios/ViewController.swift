cddea22efe577fa2211b9b54910d95d700f3a7f6
manste1n <akzhan.kaz2003@gmail.com>
2022-10-19 02:23:56 +0600

    Initial Commit

---
+++ vemo_project_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vemo_project_ios
+//
+//  Created by Акжан Калиматов on 19.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
